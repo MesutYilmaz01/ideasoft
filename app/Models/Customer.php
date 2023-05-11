0961f691043a13468fac440a13c0a769836ba62d
Mesut Yılmaz <mesut.yilmaz@stu.fsm.edu.tr>
2023-05-11 22:26:21 +0300

    Customers, products, orders models and migrations created.

---
+++ app/Models/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'since' => 'datetime:Y/m/d'
+    ];
+}
